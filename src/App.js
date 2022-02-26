e1e8198502c4b151114e79e7155d710191ad1d71
Renzo <hvrenzo@hotmail.com>
2022-02-25 22:39:07 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    count:1
+  };
+  handleClick = () => {
+    this.setState(({count}) => ({count:count+1}));
+  };
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.count}</span>
+        <br></br>
+        <button id="inc" onClick={this.handleClick}>incrementar</button>
       </div>
     );
   }
